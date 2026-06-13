4fe245efdb690b609d40bf0b9540279f72cffb2c
Saumil-Brahmbhatt <saugillgaming@gmail.com>
2026-06-13 20:56:03 +0530

    Create teams.js

---
+++ routes/teams.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const router = express.Router();
+
+const prisma = require("../lib/prisma");
+
+// GET ALL TEAMS
+router.get("/", async (req, res) => {
+    try {
+        const teams = await prisma.team.findMany();
+
+        res.json(teams);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
+});
+
+// GET TEAM BY TEAM ID
+router.get("/id/:teamId", async (req, res) => {
+    try {
+        const team = await prisma.team.findUnique({
+            where: {
+                teamId: req.params.teamId
+            }
+        });
+
+        if (!team) {
+            return res.status(404).json({
+                message: "Team not found"
+            });
+        }
+
+        res.json(team);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
+});
+
+// GET TEAM BY SLUG
+router.get("/:slug", async (req, res) => {
+    try {
+        const team = await prisma.team.findUnique({
+            where: {
+                slug: req.params.slug
+            }
+        });
+
+        if (!team) {
+            return res.status(404).json({
+                message: "Team not found"
+            });
+        }
+
+        res.json(team);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
+});
+
+// CREATE TEAM
+router.post("/", async (req, res) => {
+    try {
+        const team = await prisma.team.create({
+            data: req.body
+        });
+
+        res.status(201).json(team);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+module.exports = router;
