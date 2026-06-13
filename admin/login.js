fa0a39160d0484c89d45def497649b88ac0a61ec
Saumil-Brahmbhatt <saugillgaming@gmail.com>
2026-06-13 22:46:59 +0530

    Added Admin Panel

---
+++ admin/login.js
@@ -0,0 +1,46 @@
+async function login() {
+
+    const username =
+        document.getElementById("username").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    const response = await fetch(
+        "https://volley-api.vercel.app/api/v1/admin/login",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                username,
+                password
+            })
+        }
+    );
+
+    const data = await response.json();
+
+    if(data.token){
+
+        localStorage.setItem(
+            "token",
+            data.token
+        );
+
+        window.location.href =
+            "dashboard.html";
+
+    }else{
+
+        document.getElementById(
+            "message"
+        ).innerText =
+            data.message || "Login Failed";
+
+    }
+
+}
