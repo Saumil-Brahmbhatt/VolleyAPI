c545f38624c23a05c1ef297a93228d189b2db9d2
Saumil-Brahmbhatt <saugillgaming@gmail.com>
2026-06-14 15:50:10 +0530

    Update teams.js

--- routes/teams.js
+++ routes/teams.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const prisma = require("../lib/prisma");
+const auth = require("../middleware/auth");
 
 // GET ALL TEAMS
 router.get("/", async (req, res) => {
